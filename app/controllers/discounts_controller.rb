f1493f3903dacfb49522b3b56dfa9b56e0d8c3ad
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-08-14 11:33:36 -0300

    Change redirect after create and update an discount

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -25,7 +25,7 @@ class DiscountsController < ApplicationController
 
     respond_to do |format|
       if @discount.save
-        format.html { redirect_to @discount, notice: "Discount was successfully created." }
+        format.html { redirect_to discounts_path, notice: "Discount was successfully created." }
         format.json { render :show, status: :created, location: @discount }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class DiscountsController < ApplicationController
   def update
     respond_to do |format|
       if @discount.update(discount_params)
-        format.html { redirect_to @discount, notice: "Discount was successfully updated." }
+        format.html { redirect_to discounts_path, notice: "Discount was successfully updated." }
         format.json { render :show, status: :ok, location: @discount }
       else
         format.html { render :edit, status: :unprocessable_entity }
