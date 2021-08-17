12ea638d8b4b61d7302076b4040862a500d46947
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-08-17 00:28:47 -0300

    Add methods to StudentPlan model

--- app/models/student_plan.rb
+++ app/models/student_plan.rb
@@ -1,5 +1,21 @@
 class StudentPlan < ApplicationRecord
   belongs_to :person
-  belongs_to :tuition_fee
-  belongs_to :discount
+
+  def monthly_fee
+    current_tuition_fee.fee
+  end
+
+  def percentage
+    current_discount.discount_percentage
+  end
+
+  private
+
+  def current_tuition_fee
+    TuitionFee.where(weekly_classes: weekly_classes).order(validity: 'DESC').first
+  end
+
+  def current_discount
+    Discount.where(kind_of_plan: kind_of_plan).order(starts_at: 'DESC').first
+  end
 end
