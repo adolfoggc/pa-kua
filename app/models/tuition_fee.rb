5298f922bdc0f4ff7fb705ebe4267397460d400c
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-08-17 00:28:00 -0300

    Add method to TuitionFee model

--- app/models/tuition_fee.rb
+++ app/models/tuition_fee.rb
@@ -1,3 +1,11 @@
 class TuitionFee < ApplicationRecord
   validates :weekly_classes, :fee, :validity, presence: true
+
+  def weekly_class_text
+    return "#{weekly_classes} aulas semanais" unless weekly_class.zero?
+
+    return 'Plano Livre' if weekly_class.zero?
+
+    'Plano Inválido'
+  end
 end
