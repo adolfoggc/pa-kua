7e2ac2aaf9c6c2789aabc601d1a0095f984a6551
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-08-20 15:04:44 -0300

    Refactoring weekly_class_text

--- app/models/tuition_fee.rb
+++ app/models/tuition_fee.rb
@@ -2,9 +2,9 @@ class TuitionFee < ApplicationRecord
   validates :weekly_classes, :fee, :validity, presence: true
 
   def weekly_class_text
-    return "#{weekly_classes} aulas semanais" unless weekly_class.zero?
+    return "#{weekly_classes} aulas semanais" unless weekly_classes.zero?
 
-    return 'Plano Livre' if weekly_class.zero?
+    return 'Plano Livre' if weekly_classes.zero?
 
     'Plano Inválido'
   end
