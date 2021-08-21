2663a55e42dca35dccf9e9addf44b3a17078a73f
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-08-21 20:04:51 -0300

    Refactor br_month

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -43,8 +43,7 @@ module ApplicationHelper
     months_hash = { '1': 'janeiro', '2': 'fevereiro', '3': 'março',
                     '4': 'abril', '5': 'maio', '6': 'junho', '7': 'julho',
                     '8': 'agosto', '9': 'setembro', '10': 'outubro',
-                    '11': 'novembro', '12': 'dezembro'
-                  }
+                    '11': 'novembro', '12': 'dezembro' }
     months_hash[month.to_s.to_sym].capitalize
   end
 end
