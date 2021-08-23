366d7d1f2ea8d8b3cea877b67d96eda47900d15a
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-08-23 16:49:59 -0300

    Update members view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
     @students = []
     @instructors = []
     @traveling_instructor = []
-    Person.all.each do |p|
+    Person.all.order(id: 'ASC').each do |p|
       @students << p if p.pakua_student?
       @instructors << p if p.pakua_instructor?
       @traveling_instructor << p if p.traveling_instructor?
