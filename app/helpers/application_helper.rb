c1b59d96c39245593dc1a485773e1028028302f6
Carlos Adolfo <adolfo.ggc@gmail.com>
2021-08-31 07:40:03 -0300

    Add helpers to belt's attributes names

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -46,4 +46,44 @@ module ApplicationHelper
                     '11': 'novembro', '12': 'dezembro' }
     months_hash[month.to_s.to_sym].capitalize
   end
+
+  def belt_color_style(color)
+    case color
+    when 'branca'
+      'background-color:white !important; color: black !important; border: 1px black solid;'
+    when 'amarela'
+      'background-color:yellow !important; color: black !important; border: 1px black solid;'
+    when 'laranja'
+      'background-color:orange !important; color: white !important; border: 1px orange solid;'
+    when 'verde'
+      'background-color:green !important; color: white !important; border: 1px green solid;'
+    when 'cinza'
+      'background-color:gray !important; color: white !important; border: 1px gray solid;'
+    when 'azul'
+      'background-color:blue !important; color: white !important; border: 1px blue solid;'
+    when 'vermelha'
+      'background-color:red !important; color: white !important; border: 1px red solid;'
+    else
+      'background-color:black !important; color: white !important; border: 1px black solid;'
+    end
+  end
+
+  def belt_color_name(color)
+    return color.humanize unless color.humanize.include?(' ')
+
+    color = color.humanize.split
+    color[0].capitalize + ' ' + color[1].upcase
+  end
+
+  def modality_name(modality)
+    modality = modality.humanize
+    return modality unless modality.include?(' ')
+
+    modality = modality.split
+    modality_name = ''
+    modality.each do |m|
+      modality_name += m.capitalize + ' '
+    end
+    modality_name
+  end
 end
