23b90a77a7dae7d2529fbb6ff60ee26aa8e162ef
Carlos Adolfo <adolfo.ggc@gmail.com>
2022-02-11 13:42:52 -0300

    Remove admin_lte3

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,22 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link jquery.min.js
-//= link jquery-ui.min.js
-//= link bootstrap.bundle.min.js
-//= link Chart.min.js
-//= link sparkline.js
-//= link jquery.vmap.min.js
-//= link jquery.vmap.usa.js
-//= link jquery.knob.min.js
-//= link moment.min.js
-//= link daterangepicker.js
-//= link tempusdominus-bootstrap-4.min.js
-//= link summernote-bs4.min.js
-//= link jquery.overlayScrollbars.min.js
-//= link adminlte.js
-
-
-
 
 //= link_directory ../javascripts/sb_admin2
 //= link_directory ../stylesheets/sb_admin2
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,36 +11,3 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w[
-  font_awesome_all.min.css
-  tempusdominus-bootstrap-4.min.css
-  icheck-bootstrap.min.css
-  jqvmap.min.css
-  adminlte.min.css
-  OverlayScrollbars.min.css
-  daterangepicker.css
-  summernote-bs4.min.css
-
-  adminlte.js
-  bootstrap.bundle.min.js
-  jquery.min.js
-  jquery-ui.min.js
-  Chart.min.js
-  sparkline.js
-  jquery.vmap.min.js
-  jquery.vmap.usa.js
-  jquery.knob.min.js
-  moment.min.js
-  daterangepicker.js
-  tempusdominus-bootstrap-4.min.js
-  summernote-bs4.min.js
-  jquery.overlayScrollbars.min.js
-  select2.full.js
-  jquery.bootstrap-duallistbox.min.js
-  jquery.inputmask.min.js
-  daterangepicker.js
-  bootstrap-colorpicker.min.js
-  bootstrap-switch.min.js
-  bs-stepper.min.js
-  dropzone.min.js
-]
