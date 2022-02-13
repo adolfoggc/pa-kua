9cdc888e1edbc9ffe5c2c62f0e8db6fad1af1213
Carlos Adolfo <adolfo.ggc@gmail.com>
2022-02-13 11:51:42 -0300

    Add layout selector to dashboard_controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,12 +2,19 @@
 
 # General dashboard actions
 class DashboardController < ApplicationController
-  layout 'example_sb_admin2', only: :example
+  layout :set_view_layout
 
   def index
     get_people_data
   end
 
   def example
+  end
+
+  private
+  def set_view_layout
+    return 'example_sb_admin2' if action_name == 'example'
+
+    'sb_admin2'
   end
 end
